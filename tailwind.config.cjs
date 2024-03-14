b66bad0eed44ea8fb2fc65b5383ec157a950a414
nangonz <nangonz91@gmail.com>
2024-03-14 20:24:02 -0300

    change sections text color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
                 wiggle: {
                     '0%, 100%': { transform: 'rotate(-3deg)' },
                     '50%': { transform: 'rotate(3deg)' },
-                }
+                },
             },
             animation: {
                 wiggle: 'wiggle 1s ease-in-out infinite',
@@ -15,11 +15,11 @@ module.exports = {
             colors: {
                 light: {
                     fondo: {
-                        primary: "#fafbfc",
+                        primary: "#f9f2eb",
                         secondary: "#fff",
                     },
                     texto: {
-                        primary: "#e64e4e",
+                        primary: "#43A1F1",
                         secondary: "#272727",
                     },
                     gray: {
